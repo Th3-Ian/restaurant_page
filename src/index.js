8be9af350ce23dabd22e81ae248cbe5572a1ad45
Ian Hart <ianhart13@gmail.com>
2021-09-27 16:22:34 -0400

    forEach loop created to add menu anchors & classes dynamically

--- src/index.js
+++ src/index.js
@@ -11,52 +11,35 @@ const navBar = () => {
 	const containerDiv = document.getElementById('content')
 	const div = document.createElement('div');
 	const nav = document.createElement('nav');
+
   // Lodash, now imported by this script
-	nav.innerHTML = '<a class="tab-1 home-page">Home</a><a class="tab menu-page">Menu</a><a class="tab contact-page">Contact</a>';
+	const navItems = ['Home', 'Menu', 'Contact'];
+	nav.innerHTML = '<a class="tab home-page">Home</a><a class="tab menu-page">Menu</a><a class="tab contact-page">Contact</a>';
 
 	div.appendChild(nav);
 
 	containerDiv.appendChild(div);
+
+	//CREATE FOR EACH FUNC TO GENERATE NAV ITEMS
+	navItems.forEach(item => {
+		let navBtn = String(item);
+		const anchor = document.createElement('a');
+		anchor.innerHTML = `${navBtn}`;
+		anchor.classList.add(`${navBtn.toLowerCase()}-page`);
+		nav.appendChild(anchor);
+	});
+
+	//CREATE 2nd FOR EACH FUNC TO ADD EVENT LISTENER TO NAV ITEMS
+	/*
   nav.addEventListener('click', helloClick);
 
 	function helloClick() {
 		console.log('hello this click works');
 	}
+	*/
 }
 
 navBar();
 homePage();
 
 
-/*
-document.addEventListener('tabContent', function(){
-	const tabs = document.getElementsByClassName('tab');
-	for(let i = 0; i < tabs.length; i++) {
-		tabs[i].addEventListener('click', changeTab);
-	}
-
-
-	function changeTab() {
-		console.log('this is working');
-
-		if (this.classList.contains('home-page')) {
-			clear();
-			homePage();
-		} else if(this.classList.contains('menu-page')) {
-			clear();
-			menuPage();
-		}
-
-		document.getElementsByClassName('active')[0].classList.remove('active');
-		this.classList.add('active');
-
-		document.getElementsByClassName('live-page')[0].classList.remove('live-page');
-
-		const arrayTabs = Array.prototype.slice.call(tabs);
-		const index = arrayTabs.indexOf(this);
-		document.getElementsByClassName('panel')
-		[index].classList.add('live-page');
-
-	}
-})
-*/
